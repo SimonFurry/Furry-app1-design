614828503a98a1e05408a4571d54dc161ea73c7c
Simon <simonfurry@gmail,.com>
2021-10-24 12:57:09 -0400

    Test case Pseudo-Code

---
+++ Furry-app1-design/src/test/java/ApplicationTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicationTest {
+
+    @Test
+    void createToDoTest(){
+        /*
+        Generate file with pre-existing conditions
+        FileName with less than and more than 3 character to test "must be 3 char long" limiter
+        Format: name,description,YYYY-MM-DD,(Create 3 things to do)
+         */
+    }
+
+    @Test
+    void deleteToDoTest(){
+        /*
+        Create test file then run delete code, scan for
+        file name if!=FALSE success
+         */
+    }
+
+    @Test
+    void editToDoTest(){
+        /*
+        Test functionality of editing a pre-existing file
+        and changing the data to pre-existing parameters
+         */
+    }
+
+    @Test
+    void saveToDoTest(){
+        /*
+        Test functionality with default save location and save parameters
+         */
+    }
+}
